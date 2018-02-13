5aae2a45890829911a07d680e0dd23257dd0903a
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-13 01:20:05 -0300

    using vecotr

--- neuralnet.cpp
+++ neuralnet.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
-#include<array>
+#include<vector>
 #include<random>
+#include <cstdlib>
 
 using namespace std;
 
 class Neuralnet{    
 
 public:
-    array<float, 2> weights;
+    vector<float> weights;
+
     Neuralnet(){
         int valor = 1;
         for (int i = 0; i < weights.size(); i++){
@@ -24,12 +26,17 @@ public:
     }
 };
 
+float get_random()
+{
+    static std::default_random_engine e;
+    static std::uniform_real_distribution<> dis(-1, 1); // rage 0 - 1
+    return dis(e);
+}
 
 int main(){
-
     Neuralnet *n = new Neuralnet();
-    n->get_1();
-
+    //n->get_1();
+    cout << get_random();
 
     return 0;
 }
