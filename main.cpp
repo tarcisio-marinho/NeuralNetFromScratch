f53a6c3f6394f5a2bd2dc2b54fea07c6d7442e90
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-13 03:26:18 -0300

    PERCEPTRON WORKING MOTHERFUCKER HOLY SHIT

--- main.cpp
+++ main.cpp
@@ -6,26 +6,25 @@ void create_dataset(int size, Perceptron *n){
     vector<Point*> points;
 
     for (int i = 0; i<size; i++){
-        points[i] = new Point();
+        points.push_back(new Point());
+    }
+
+    for (Point *p : points){
+        cout << p->x << " " << p->y << " " << p->label << "\n";
     }
 
     // for (Point *p : points){
-    //     cout << p->x << " " << p->y << " " << p->label << "\n";
+    //     n->train({p->x, p->y}, p->label);
     // }
 
-    for (Point *p : points){
-        n->train({p->x, p->y}, p->label);
-    }
-
-    cout << n->guess({114, 216});
+    // cout << n->guess({114, 216});
 }
 
 
 int main(int argc, char * argv[]){
     
     Perceptron *n = new Perceptron(2);
-
-    create_dataset(2, n);
+    create_dataset(5, n);
 
     return 0;
 }
