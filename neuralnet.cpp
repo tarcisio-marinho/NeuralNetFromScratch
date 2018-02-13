066bdde5223c489ebd1d1003917bf65aeeca5e8c
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-13 01:33:36 -0300

    start getting shape

--- neuralnet.cpp
+++ neuralnet.cpp
@@ -11,32 +11,35 @@ public:
     vector<float> weights;
 
     Neuralnet(){
+        srand (static_cast <unsigned> (time(0)));
         int valor = 1;
         for (int i = 0; i < weights.size(); i++){
-            weights[i] = valor;
-            valor ++;
+
         }
     }
     
-    void get_1(){
-        for (float n : weights){
-            cout << n << "\n";
+    int sign(float number){
+        if(number < 0){
+            return -1;
         }
-
+        return 1;
     }
+
+    float get_random(){
+        return -1 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(1-(-1))));
+    }
+
 };
 
-float get_random()
-{
-    static std::default_random_engine e;
-    static std::uniform_real_distribution<> dis(-1, 1); // rage 0 - 1
-    return dis(e);
-}
+
 
 int main(){
+    
     Neuralnet *n = new Neuralnet();
-    //n->get_1();
-    cout << get_random();
 
+    for (int i = 0; i<10 ;i ++){
+        cout << n->get_random() << "\n";
+    }
+    
     return 0;
 }
