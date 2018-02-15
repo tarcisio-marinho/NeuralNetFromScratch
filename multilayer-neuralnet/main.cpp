287c65f4cb5a607cec627b684177154c6e480726
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-14 22:33:58 -0300

    adding activation function

--- multilayer-neuralnet/main.cpp
+++ multilayer-neuralnet/main.cpp
@@ -2,11 +2,14 @@
 #include "matrix.h"
 #include<vector>
 #include<iostream>
+#include<cmath>
 
 int main(int argc, char *argv[]){
 
     NeuralNet *n = new NeuralNet(2, 2, 1);
-    
+    Matrix *m = new Matrix(2, 2);
+    n->feedfoward(m);
+
     
     // float (*foo)(float);
     // foo = &pi;
--- multilayer-neuralnet/neuralnet.h
+++ multilayer-neuralnet/neuralnet.h
@@ -3,6 +3,7 @@
 #define __NEURALNET__
 
 #include "matrix.h"
+#include<cmath>
 
 class NeuralNet{
 public:
@@ -12,11 +13,32 @@ public:
         n_inputs = inputs;
         n_hidden = hidden;
         n_outputs = outputs;
+        
+        // weights matrixes
         weights_ih = new Matrix(n_hidden, n_inputs);
         weights_ho = new Matrix(n_outputs, n_hidden);
+
+        // biases 
         bias_h = new Matrix(n_hidden, 1);
         bias_o = new Matrix(n_outputs, 1);
 
+    }
+
+    float feedfoward(Matrix * input){
+
+        // Generating the hidden outputs
+        Matrix * hidden = weights_ih->matmul(input);
+        hidden->matadd(bias_h);
+
+        // Activation function
+        float (*sigmoid_func)(float);
+        sigmoid_func = &sigmoid;
+        hidden->map(sigmoid_func);
+    }
+
+    // activation function
+    float sigmoid(float x){
+        return 1/(1 + exp(-x));
     }
 
 
