5d96ea4333edea18d84cd8a28bf83fea7c62ecaf
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-11-04 23:59:47 -0300

    dando pau mais a frente

--- MLP/main.cpp
+++ MLP/main.cpp
@@ -8,13 +8,13 @@
 
 class Data{
 public:
-    Data(std::vector<std::vector<float> > x, std::vector<float> y){
+    Data(std::vector<std::vector<float> > x, std::vector<std::vector<float> > y){
         this->x = x;
         this->y = y;
     }
 
     std::vector<std::vector<float> > x;
-    std::vector<float> y;
+    std::vector<std::vector<float> > y;
 };
 
 
@@ -37,7 +37,7 @@ std::vector<std::string> split(const std::string& s, const char& c)
 
 Data* get_data(){
     std::vector<std::vector<float> > x;
-    std::vector<float> y;
+    std::vector<std::vector<float> > y;
 
 
     std::ifstream file("data/breast-cancer-wisconsin.data");
@@ -50,7 +50,9 @@ Data* get_data(){
             ret.erase(ret.begin()); //  removes ID
 
             int value = float(atoi(ret[ret.size()-1].c_str())); // get the class
-            y.push_back(value);
+            std::vector<float> aux1;
+            aux1.push_back(value);
+            y.push_back(aux1);
             ret.erase(ret.begin() + 9); // remove the class
             
             // cleaning the '?' data
@@ -79,7 +81,8 @@ void print_data_set(Data *d){
         for(int a : n){
             std::cout << a << ",";
         }
-        std::cout << d->y[contador] << std::endl; 
+
+        std::cout << d->y[contador][0] << std::endl; 
         contador++;
     }
 }
@@ -89,25 +92,22 @@ int main(int argc, char *argv[]){
 
     // get the dataset    
     Data* d = get_data();
-
-    // create the input matrix
-    Matrix *training_dataset = new Matrix(d->x);
-
-    // labels
-    Matrix *labels = Matrix::fromArray(d->y);
-
-    NeuralNet *n = new NeuralNet(9, 20, 2);
+    //print_data_set(d);
+    
+    NeuralNet *nn = new NeuralNet(9, 20, 2);
     
     // testing 
-     Matrix * test = Matrix::fromArray(std::vector<float>{1, 1, 2, 3, 4, 5, 1, 2, 3});
-     Matrix * retorno = n->predict(test);
-     retorno->print();
+    Matrix * test = Matrix::fromArray(std::vector<float>{1, 1, 2, 3, 4, 5, 1, 2, 3});
+    Matrix * retorno = nn->predict(test);
+    retorno->print();
 
-    n->fit(training_dataset, labels);
+    int contador = 0;
+    for (auto x : d->x){
+        Matrix * input = Matrix::fromArray(x);
+        Matrix *label = Matrix::fromArray(d->y[contador]);
+        nn->fit(input, label);
+    }
 
 
-    //Matrix *output = n->predict(new Matrix(1, 1));
-   // output->print();
-    
 
 }
--- MLP/neuralnet.cpp
+++ MLP/neuralnet.cpp
@@ -1,5 +1,7 @@
 #include "neuralnet.h"
-
+void print(int a){
+    std::cerr << a <<std::endl;
+}
 NeuralNet::NeuralNet(int inputs, int hidden, int outputs, float lr){
     this->n_inputs = inputs;
     this->n_hidden = hidden;
@@ -48,64 +50,76 @@ void NeuralNet::fit(Matrix * inputs, Matrix * targets){
     /*  FeedFoward
         Input -> Hidden
     */
-
+    print(1);   
     // Generating the hidden layer outputs
     Matrix *hidden_layer_output = weights_ih->matmul(inputs);
-    hidden_layer_output->matadd(bias_h); // PAU
-    
+    print(2);
+    hidden_layer_output->matadd(bias_h); 
+    print(3);
     // Activation function
     hidden_layer_output->map(apply_function);
-
+    print(4);
     /*
         Hidden -> Ouput
     */
 
     // Generating the output layer output
     Matrix* output_layer_output = weights_ho->matmul(hidden_layer_output);
+    print(5);
     output_layer_output->matadd(bias_o);
-
+    print(6);
     // Activation function
     output_layer_output->map(apply_function);
-
+    print(7);
 
     /*
         BackPropagation
     */
-
+    print(8);
     // Calculate the erro -> ERROR = TARGET - OUTPUTS
     Matrix * output_errors = targets->matsub(output_layer_output);
-
+    print(9);
     
     // Calculate gradient
     Matrix * gradients = Matrix::map_static(output_layer_output, apply_function2);
+    print(10);
     gradients->matmul(output_errors);
+    print(11);
     gradients->mul(learning_rate);
-
+    print(12);
     // Calculate deltas
     Matrix * hidden_transposed = hidden_layer_output->transpose();
+    print(13);
     Matrix * weight_ho_deltas = gradients->matmul(hidden_transposed);
-
+    print(14);
     // adjust the weights by deltas
     weights_ho->matadd(weight_ho_deltas);
+    print(15);
     // adjust the bias by its deltas
     bias_o->matadd(gradients);
-
+    print(16);
 
     // Calculate the hidden layer errors
     Matrix * weights_ho_transposed = weights_ho->transpose();
+    print(17);
     Matrix * hidden_errors = weights_ho_transposed->matmul(output_errors);
-
+    print(18);
     // Calculate hidden gradient
     Matrix * hidden_gradient = Matrix::map_static(hidden_layer_output, apply_function2);
-    hidden_gradient->matmul(hidden_errors);
+    print(19);
+    hidden_gradient->matmul(hidden_errors); // PAU
+    print(20);
     hidden_gradient->mul(learning_rate);
-
+    print(21);
     // Calculate input -> hidden deltas
     Matrix * inputs_transposed = inputs->transpose();
+    print(22);
     Matrix * weight_ih_deltas = hidden_gradient->matmul(inputs_transposed);
-
+    print(23);
     weights_ih->matadd(weight_ih_deltas);
+    print(24);
     bias_h->matadd(hidden_gradient);
+    print(25);
 }
 
 // activation function
