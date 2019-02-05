7cbbc8e68015ac1c783b9112c7e9a9f8c6e8c169
tarcisio-marinho <tarcisio.oliveira@digiallworkers.com>
2019-02-05 14:20:14 -0300

    adding tanh

--- Python/activation_functions.py
+++ Python/activation_functions.py
@@ -6,7 +6,7 @@ import math
 
 
 def sigmoid(x):
-    return 1 / 1 + np.exp(-x) 
+    return 1 / (1 + np.exp(-x))
 
 def softmax(x):
     return np.exp(x) / np.sum(np.exp(x))
@@ -14,4 +14,7 @@ def softmax(x):
 def tanh(x):
     return (2 / 1 + np.exp(-2*x)) - 1
 
-print(softmax([3, 4, 5]), sigmoid(3))
+
+print((np.array([[1, 2, 3], [1, 2, 3]]).shape))
+
+print(softmax(np.array([3, 4, 5])), sigmoid(0))
