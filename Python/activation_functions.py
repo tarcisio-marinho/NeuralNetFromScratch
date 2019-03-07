fdb4c40314f0b5a1d5e134840ba42c1a7ee01798
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2019-03-06 23:46:15 -0300

    updating MLP python code and adding dropout script

--- Python/activation_functions.py
+++ Python/activation_functions.py
@@ -14,5 +14,18 @@ def softmax(x):
 def tanh(x):
     return (2 / 1 + np.exp(-2*x)) - 1
 
+def softmax(x):
+    e = numpy.exp(x - numpy.max(x))  # prevent overflow
+    if e.ndim == 1:
+        return e / numpy.sum(e, axis=0)
+    else:  
+        return e / numpy.array([numpy.sum(e, axis=1)]).T  # ndim = 2
+
+def ReLU(x):
+    return x * (x > 0)
+
+def dReLU(x):
+    return 1. * (x > 0)
+
 
 print(softmax(np.array([3, 4, 5])), sigmoid(-1))
--- Python/nn.py
+++ Python/nn.py
@@ -1,3 +1,50 @@
+import numpy as np
+
+class MLP():
+
+    def __init__(self, n_input, n_hidden, n_output, lr):
+        self.score = None
+        self.__n_input = n_input
+        self.__n_hidden = n_hidden
+        self.__n_output = n_output
+        self.__n_hidden_layers = 1
+        self.__lr = lr
+
+        self.__net = []
 
 
-pass
+    def __relu(x, deriv=False):
+        if(deriv):
+            return 1. * (x > 0)
+        return x * (x > 0)
+
+
+    def __softmax(x):
+        return np.exp(x) / np.sum(np.exp(x))
+
+    def fit(self, x):
+
+        pass
+
+    def predict(self, x):
+        pass
+    
+    def score(self):
+        pass
+
+    def print(self):
+        for i,layer in enumerate(self.__net,1):
+            print("Layer {} ".format(i))
+            for j,neuron in enumerate(layer,1):
+                print("neuron {} :".format(j),neuron)
+
+if __name__ == "__main__":
+
+
+
+
+    clf = MLP()
+    clf.fit()
+
+    result = clf.predict()
+    score = clf.score()
